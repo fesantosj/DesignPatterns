aa5ebc426689adddd6dcdbe57bce1d893a278807
fesantosj <tec.fesantos@gmail.com>
2022-10-21 10:51:38 -0300

    First Commit;

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace Singleton{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class LogEventos{
+        /// <summary>
+        /// Instancia
+        /// </summary>
+        private static LogEventos? _instancia;
+        public static LogEventos Instance{
+            get{
+                if(_instancia == null)
+                    _instancia = new LogEventos();                
+                return _instancia;
+            }
+        }
+        protected LogEventos(){
+
+        }
+
+        public void ImprimirConsole(string mensagem){
+            WriteLine($"Aviso! -> {mensagem}");
+        }  
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using Singleton;
+
+    Console.Title = "Singleton Pattern";
+    
+    var log = LogEventos.Instance;
+    var log2 = LogEventos.Instance;
+    
+    if((log2 is LogEventos) && log == log2)
+        log.ImprimirConsole("Vejam só, Log2 é uma instancia de LogEventos, assim como log.");    
+
+    log2.ImprimirConsole("Finalizando execução!");
+
+
+    
